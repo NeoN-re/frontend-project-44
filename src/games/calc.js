f28498e042f024869e665e474a0e14130d03103c
NeoN-re <neonchik0000@gmail.com>
2026-04-18 00:29:01 +0300

    3

---
+++ src/games/calc.js
@@ -0,0 +1,38 @@
+import runGame from '../index.js';
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const getRandomOperator = () => {
+  const operators = ['+', '-', '*'];
+  const randomIndex = Math.floor(Math.random() * operators.length);
+  return operators[randomIndex];
+};
+
+const calculate = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return null;
+  }
+};
+
+const generateRound = () => {
+  const num1 = getRandomNumber(1, 30);
+  const num2 = getRandomNumber(1, 30);
+  const operator = getRandomOperator();
+  const question = `${num1} ${operator} ${num2}`;
+  const correctAnswer = String(calculate(num1, num2, operator));
+  return { question, correctAnswer };
+};
+
+const runCalcGame = () => {
+  const gameDescription = 'What is the result of the expression?';
+  runGame(gameDescription, generateRound);
+};
+
+export default runCalcGame;
